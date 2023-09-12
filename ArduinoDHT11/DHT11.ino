e3a97333310356171b60683ee25edd3f9898567d
itsmewt <104033731+itsmewt@users.noreply.github.com>
2023-09-12 08:48:04 -0300

    Update DHT11.ino

--- ArduinoDHT11/DHT11.ino
+++ ArduinoDHT11/DHT11.ino
@@ -1,31 +1,44 @@
 #include <DHT.h>
 #include <DHT_U.h>
 
-#define Type DHT11
-int dhtPin = 2;
-DHT HT(dhtPin, Type);
+#define Tipo DHT11
+int pinoDHT = 2;
+DHT HT(pinoDHT, Tipo);
 float tempC;
-float humidity;
-int dt (10000);
+float umidade;
+
+int pinoLED = 7; 
+bool sensorAtivado = false;
+
 void setup() {
   Serial.begin(9600);
   HT.begin();
-
+  pinMode(pinoLED, OUTPUT); 
 }
 
 void loop() {
   tempC = HT.readTemperature();
+  umidade = HT.readHumidity();
+  
   Serial.println();
-  Serial.print("The temperature is: ");
+  Serial.print("A temperatura é: ");
   Serial.print(tempC);
   Serial.print("ºC ");
-  delay(dt);
   Serial.println();
   Serial.println();
   
-  humidity = HT.readHumidity();
   Serial.println();
-  Serial.print("The humidity is: ");
-  Serial.print(humidity);
+  Serial.print("A umidade é: ");
+  Serial.print(umidade);
   Serial.print("% ");
+  
+  if (!sensorAtivado && !isnan(tempC) && !isnan(umidade)) {
+    sensorAtivado = true;
+    digitalWrite(pinoLED, HIGH); 
+  } else if (sensorAtivado && (isnan(tempC) || isnan(umidade))) {
+    sensorAtivado = false;
+    digitalWrite(pinoLED, LOW); 
+  }
+
+  delay(1000); 
 }
